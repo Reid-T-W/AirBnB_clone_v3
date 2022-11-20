0cae43e255c67a2360759c3b45ce865ac2892a48
Reid-T-W <rediettadesse100@gmail.com>
2022-11-20 14:22:41 +0300

    error responses changed to abort

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -41,10 +41,10 @@ def create_state():
         state = request.get_json()
         json.dumps(state)
     except Exception:
-        return jsonify("Not a JSON\n"), 400
+        return abort(400, 'Not a JSON')
     from models.state import State
     if "name" not in state.keys():
-        return jsonify("Missing name\n"), 400
+        return abort(400, 'Missing name')
     state_obj = State(**state)
     storage.new(state_obj)
     storage.save()
@@ -57,7 +57,7 @@ def update_state(state_id):
         state = request.get_json()
         json.dumps(state)
     except Exception:
-        return jsonify("Not a JSON\n"), 400
+        return abort(400, 'Not a JSON')
     from models.state import State
     single_state = storage.get("State", state_id)
     if single_state is None:
