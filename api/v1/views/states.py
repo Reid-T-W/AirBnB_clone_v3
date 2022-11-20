d790e4e9afa3dfa9a4bb8dfa45d3f3a499f1ed16
Reid-T-W <rediettadesse100@gmail.com>
2022-11-20 20:52:27 +0300

    404 aborts uncommented

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -20,8 +20,7 @@ def states():
 def get_state(state_id):
     single_state = storage.get("State", state_id)
     if single_state is None:
-        # abort(404)
-        pass
+        abort(404)
     else:
         return jsonify(single_state.to_dict())
 
@@ -30,8 +29,7 @@ def get_state(state_id):
 def delete_state(state_id):
     single_state = storage.get("State", state_id)
     if single_state is None:
-        # abort(404)
-        pass
+        abort(404)
     else:
         storage.delete(single_state)
         storage.save()
@@ -64,8 +62,7 @@ def update_state(state_id):
     from models.state import State
     single_state = storage.get("State", state_id)
     if single_state is None:
-        # abort(404)
-        pass
+        abort(404)
     for key, value in state.items():
         if key is "id" or key is "created_at" or key is "updated_at":
             pass
